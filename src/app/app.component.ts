68f68159da473af07f4c1c05fb1d7a58cc9a8bd6
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2021-06-30 12:22:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'giphy';
+  
+  answer: any;
+  location: string;
+
+  constructor(private http: HttpClient) {}
+
+  search() {
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=NkDtt6HoC3XjS6G6K3Jsz8k83G9b5Jf6&limit=1&q=' + this.location)
+    .subscribe((answer) => {
+      this.answer = answer;
+      console.log(this.answer);
+    })
+  }
 }
